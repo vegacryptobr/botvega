b51a5b2d4dba207c496b094fa382e92f505156ff
vegacryptobr <reni@vegacrypto.xyz>
2023-11-13 11:07:45 -0300

    commit

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,27 @@
 import { AuthProvider } from '../context/authContext';
 import React from 'react';
 import { AppProps } from 'next/app';
+import Script from "next/script";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Component {...pageProps} />
     </AuthProvider>
   );
