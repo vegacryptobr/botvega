035e61eb7eff11827b46f1c0b785c51f72b6f46d
vegacryptobr <reni@vegacrypto.xyz>
2023-11-13 11:56:54 -0300

    commit

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,12 +1,13 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'VegaChat',
-  description: 'Conheça o VegaChat, o seu assistente virtual que vai te ajudar a entender o Real Digital(Drex)! Deixe o VegaChat guiá-lo por uma jornada informativa e empolgante sobre esta inovação do Banco Central do Brasil. Descubra o que é, como funciona e o impacto no cenário financeiro. Deixe-nos ser a sua fonte confiável de informações. Venha explorar o mundo do Drex com o VegaChat hoje mesmo!',
+  description: 'VegaChat é o seu assistente virtual que te ajudar a entender o Real Digital (Drex)! Deixe o VegaChat guiá-lo por uma jornada informativa e empolgante sobre esta inovação do Banco Central do Brasil. Descubra o que é, como funciona e o impacto no cenário financeiro. Deixe-nos ser a sua fonte confiável de informações. Venha explorar o mundo do Drex com o VegaChat hoje mesmo!',
 }
 
 export default function RootLayout({
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
